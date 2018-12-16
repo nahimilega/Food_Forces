ff85bd669a817d9744ab8f4fc44c31c2add67222
nahimilega <archit18221@iiitd.ac.in>
2018-12-16 21:01:39 +0530

    Addes some description of the order

--- foodforces/mainpage/views.py
+++ foodforces/mainpage/views.py
@@ -5,8 +5,10 @@ from .models import user, order
 def index(request):
     
     all_user=user.objects.all()
+    all_order=order.objects.all()
     a={
       'all_user':all_user,
+      'all_order':all_order,
     }
 
     return render(request,"index.html",a)
